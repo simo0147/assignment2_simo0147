0db40d002850490cedbb38b3bcf700dfcc0d0730
Aidan Simons <simo0147@algonquinlive.com>
2021-03-27 13:52:02 -0400

    submit

---
+++ script.js
@@ -0,0 +1,21 @@
+const row1 = document.querySelector('.row1')
+
+const $row1 = document.getElementById('row1')
+row1.innerHTML = 
+'<img src="images/e30/e30_1.jpg" alt="e30_1"> <img src="images/e30/e30_2.jpg" alt="e30_1">,<img src="images/e30/e30_3.jpg" alt="e30_1">,<img src="images/e30/e30_4.jpg" alt="e30_1">'
+
+
+
+const row2 = document.querySelector('.row2')
+
+const $row2 = document.getElementById('row2')
+row2.innerHTML = 
+'<img src="images/e36/e36_1.jpg" alt="e3 6_1"><img src="images/e36/e36_2.jpg" alt="e36_2">,<img src="images/e36/e36_3.jpg" alt="e36_3">,<img src="images/e36/e36_4.jpg" alt="e36_4">'
+
+
+
+const row3 = document.querySelector('.row3')
+
+const $row3 = document.getElementById('row3')
+row3.innerHTML = 
+'<img src="images/e46/e46_1.jpg" alt="e46_1">,<img src="images/e46/e46_2.jpg" alt="e46_2">,<img src="images/e46/e46_3.jpg" alt="e46_3">,<img src="images/e46/e46_4.jpg" alt="e46_4">'
